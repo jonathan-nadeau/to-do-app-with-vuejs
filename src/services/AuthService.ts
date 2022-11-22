75d070ed9b640e1c1c9ddb32db6fd70604133251
jnadeau22 <jnadeau@mcjconseil.com>
2022-11-21 22:17:33 -0500

    implement structure of auth service class

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,5 +1,14 @@
-class AuthService {
+import { API_TOKEN, API_URL } from "@/constants";
 
+class AuthService {
+  url = API_URL;
+  token = API_TOKEN;
+
+  constructor() {}
+
+  login(/* object to be defined */) {}
+  logout() {}
+  getUser(id: number) {}
 }
 
-export default AuthService
+export default AuthService;
