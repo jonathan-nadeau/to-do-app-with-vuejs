30cdb3dd83bb4f39d4d798bda09a9c56f4e3e0fe
jnadeau22 <jnadeau@mcjconseil.com>
2022-11-28 21:27:34 -0500

    add authService class

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { AuthService } from "@/services";
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const authService = new AuthService();
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
+      meta: {
+        requiresAuth: true,
+      },
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  ],
+});
 
-export default router
+router.beforeEach(async (to, from) => {
+  const userIsAuthenticate = await authService.authenticate();
+
+  if (!userIsAuthenticate.success) return "/login";
+});
+
+export default router;
